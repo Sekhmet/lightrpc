0d790ef3465bca1d8e30a1a87c6a1177dfdc5a6b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-06-28 19:04:28 +0200

    update rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 const { BUILD_TYPE, NODE_ENV } = process.env;
 
@@ -9,15 +9,19 @@ const config = {
   plugins: [],
 };
 
+const babelConfig = {
+  plugins: ['external-helpers'],
+  exclude: 'node_modules/**',
+};
+
 if (BUILD_TYPE === 'cjs') {
   config.output = { format: 'cjs' };
-  config.plugins.push(nodeResolve(), babel());
+  config.plugins.push(nodeResolve(), babel(babelConfig));
 }
 
 if (BUILD_TYPE === 'umd') {
-  config.output = { format: 'umd' };
-  config.name = 'LightRPC';
-  config.plugins.push(babel({ exclude: 'node_modules/**' }));
+  config.output = { format: 'umd', name: 'LightRPC' };
+  config.plugins.push(babel(babelConfig));
 }
 
 if (NODE_ENV === 'production') {
